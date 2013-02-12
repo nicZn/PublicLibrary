88f11f27712f515f8dc06c4c497d5423eb2f19f7
n_cho <n_cho@yumemi.co.jp>
2013-02-12 17:40:25 +0900

    add sql and http

---
+++ PublicLibrary/NetWork/NZHttpRequest.h
@@ -0,0 +1,13 @@
+//
+//  NZHttpRequest.h
+//  PublicLibrary
+//
+//  Created by 張 寧 on 2013/02/12.
+//  Copyright (c) 2013年 張 寧. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NZHttpRequest : NSObject
+
+@end
